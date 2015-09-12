58dceacacc4c6da57f45fc8e4ca14a9232ac566e
lhx15980 <linhangxin@linhangxindeMacBook-Air.local>
2015-09-12 22:56:40 +0800

    add main

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c
+//
+//  Created by 林航鑫 on 15/5/31.
+//  Copyright (c) 2015年 林航鑫. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
